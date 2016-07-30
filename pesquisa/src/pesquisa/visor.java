c34e586a63def69d27798eaf8692ab2cbda922ba
Alex Alves <alex7alves10@hotmail.com>
2016-07-30 11:31:57 -0300

    Compilando com nomes de pastas com espaços

--- pesquisa/src/pesquisa/visor.java
+++ pesquisa/src/pesquisa/visor.java
@@ -237,7 +237,7 @@ public class visor extends javax.swing.JFrame {
         try{
       
           
-            String con2 = "cd C:\\QuoGol-IDE\\IDE\\portugol-master && lua53 compila.lua "+caminho+ " > novo ";
+            String con2 = "cd C:\\QuoGol-IDE\\IDE\\portugol-master && lua53 compila.lua "+"\""+caminho+"\""+ " > novo ";
             Process p = Runtime.getRuntime().exec("cmd /c" +con2 );
             p.waitFor();
          
@@ -266,7 +266,7 @@ public class visor extends javax.swing.JFrame {
                 }
             
             }else {
-                con2 = "cd C:\\QuoGol-IDE\\IDE\\portugol-master && lua53 compila.lua "+caminho+ " 2> novo ";
+                con2 = "cd C:\\QuoGol-IDE\\IDE\\portugol-master && lua53 compila.lua "+"\""+caminho+"\""+ " 2> novo ";
                 Process p2 = Runtime.getRuntime().exec("cmd /c" +con2 );
             
             p2.waitFor();
@@ -398,7 +398,7 @@ public class visor extends javax.swing.JFrame {
          if(compilado == true) {
             try {
         
-                String tudo = "cd C:\\QuoGol-IDE\\IDE\\portugol-master && echo @ title Executando > compilar.bat  && echo  @ lua53 main2.lua "+caminho+" >> compilar.bat && echo @ pause >> compilar.bat && echo  @ taskkill /f /im cmd.exe >> compilar.bat ";
+                String tudo = "cd C:\\QuoGol-IDE\\IDE\\portugol-master && echo @ title Executando > compilar.bat  && echo  @ lua53 main2.lua "+"\""+caminho+"\""+" >> compilar.bat && echo @ pause >> compilar.bat && echo  @ taskkill /f /im cmd.exe >> compilar.bat ";
                 Process p = Runtime.getRuntime().exec("cmd /c" +tudo );    
         
                 p.waitFor();
